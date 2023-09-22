307710e33afa87832c976d6048e1d7ea49352ca1
ChandraMounica <mouni.sannuthi@gmail.com>
2023-09-22 01:07:23 -0500

    Two sum in single pass

---
+++ twoSumHashing.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+
+    let hashMap = {};
+
+    for(let i=0; i<nums.length; i++){
+
+        if(nums[i] in hashMap){
+            return [hashMap[nums[i]],i];
+        }
+        hashMap[target-nums[i]] = i;
+    }
+    
+};
+
+console.log(twoSum([2,7,11,15],9));
